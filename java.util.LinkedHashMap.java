1cb881ccfe54afcbfea1b756c548befd4af9e3fe
Yadong Wen <ralph.wen@gmail.com>
2014-07-02 22:01:07 +0800

    Created java.util.LinkedHashMap.java

---
+++ java.util.LinkedHashMap.java
@@ -0,0 +1,6 @@
+##java.util.LinkedHashMap.java.md
+----------
+
+Hash table and linked list implementation of the `Map` interface, with predictable iteration order.
+
+A doubly-linked list is maintained running thru all of the entries. The iteration order is normally the insertion order. Note the order is not affected when the key is re-inserted.
